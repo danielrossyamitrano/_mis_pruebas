e0cc46d1b6064b3678ef52552ebde6910b708149
zenieldanaku <danielrossyamitrano@gmail.com>
2016-02-11 15:57:41 -0300

    Parallel Node: + nuevo tipo de Composite experimental, basado en un post que encontré en:   http://guineashots.com/2014/08/10/an-introduction-to-behavior-trees-part-2/   También vi algo parecido en la documentación del Unreal Engine 4   Encontré, además, posts que hablan de un cuarto estado "interrupted" y un Selector activo.   Quizás esas cosas sean implementadas en el futuro.

--- behavior trees/core/composites.py
+++ behavior trees/core/composites.py
@@ -1,21 +1,18 @@
 from .status import *
 from .node import Node
 
-__all__ = ['Composite', 'Secuence', 'Selector']
+__all__ = ['Composite', 'Secuence', 'Selector', 'Parallel']
 
 
 class Composite(Node):
     type = 'Composite'
     children = None
     current_id = None
-    chld = None
 
     def __init__(self, tree, idx, children):
         # these are NOT containers, they just point to their children
-        self.children = []  # to prevent overriding
         super().__init__(tree, idx)
-        for child in children:
-            self.children.append(child)
+        self.children = [child for child in children]
         self.current_id = 0
 
     def update(self):
@@ -67,3 +64,39 @@ class Selector(Composite):
 
         elif status is Success:
             self.tree.reset_to_check()
+            
+class Parallel(Composite):
+    # based on http://guineashots.com/2014/08/10/an-introduction-to-behavior-trees-part-2/
+    
+    name = 'Parallel'
+    children_status = []
+    success_value = 0
+    failure_value = 0
+    
+    def __init__(self, tree, idx, children, S=0, F=0):
+        super().__init__(tree,idx,children)
+        self.children_status = [0 for i in children]
+        self.success_value = S
+        self.failure_value = F
+        
+    def __repr__(self):
+        lista = [str(c.idx) for c in self.children]
+        return ' '.join([self.type, '#' + str(self.idx), self.name, '(' + ', '.join(lista) + ')'])
+    
+    def update(self):
+        for child in self.children:
+            child.update()
+        self.current_id = 0
+        
+    def get_child_status(self, status):
+        self.children_status[self.current_id] = status
+        self.current_id += 1
+        if self.children_status.count(Success) >= self.success_value:
+            self.parent.get_child_status(Success)
+            
+        elif self.children_status.count(Failure) >= self.failure_value:
+            self.parent.get_child_status(Failure)
+            
+        else:
+            self.parent.get_child_status(Running)
+    
