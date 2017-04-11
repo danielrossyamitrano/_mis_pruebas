4c923a9b37456c7e53f3203dcda1d0f7cfc8daad
zenieldanaku <danielrossyamitrano@gmail.com>
2017-04-11 15:21:20 -0300

    Nueva prueba: ReversibleDict Se trata de un objeto que contiene pares de valores. Puede ser accesado como un dict, usando uno de los valores de un par como key, devolviendo el otro valor como value.

---
+++ Reversible Dict/clases.py
@@ -0,0 +1,25 @@
+class ReversibleDict:
+    """This object stores pairs of values. Both values from each pair have to be hashable. The object may be acceded by key, where the key is one of the values of a pair. Each pair has to be unique."""
+    
+    def __init__(self):
+        pass
+        
+    def __getitem__(self,key):
+        pass
+        
+    def __setitem__(self,key,value):
+        pass
+    
+    def __delitem__(self,key):
+        pass
+    
+    def __contains__(self,key):
+        pass
+    
+    def __repr__(self):
+      pass
+
+class ValuePair:
+    def __init__(self,value_a,value_b):
+        pass
+    
