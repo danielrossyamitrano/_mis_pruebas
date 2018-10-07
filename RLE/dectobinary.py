c593d243587e9bd08f135fc4d7fa7b9b731f461a
zenieldanaku <danielrossyamitrano@gmail.com>
2018-10-07 13:35:09 -0300

    Clean-up - Eliminados varios módulos debido a que, o bien eran pruebas completas (las   versiones finales se pueden encontrar en versiones anteriores de este repo),   o eran pruebas fallidas que nunca dieron resultado. * dectobinary: b() ahora se llama to_binary() y fue drásticamente simplificada;   se agregaron también tres nuevas funciones: to_decimal(), alphatobin() y   bintoalpha().

--- RLE/dectobinary.py
+++ RLE/dectobinary.py
@@ -1,36 +1,32 @@
-def b(n):
-    b = ''
-    if n == 0:
-        f = '0'
-    else:
-        while n > 0:
-            r = n%2
-            n = n//2
-            b += str(r)
-        l = list(b)
-        l.reverse()
-        f = ''.join(l)
-    return f
+def to_binary(decimal, size=8):
+    seq = []
+    while decimal > 0:
+        seq.append(str(decimal % 2))
+        decimal //= 2
+
+    # add leading 0s to fit size
+    seq += ['0'] * (size - len(seq))
+
+    # reverse, join and return
+    return ''.join(reversed(seq))
+
+
+def to_decimal(b):
+    n = 0
+    for i, s in enumerate(reversed(b)):
+        n += ((2 ** i) * int(s))
+    return n
+
 
 def alphatobin(char):
-    L = 'abcdefghijklmnñopqrstuvwxyz '
-    i = L.index(char)
-    d = b(i)
+    letters = 'abcdefghijklmnñopqrstuvwxyz '
+    i = letters.index(char)
+    d = to_binary(i)
     return d
 
-def bintoalpha(code):
-    L = 'abcdefghijklmnñopqrstuvwxyz '
-    dec = d(code)
-    i = L[dec]
-    return i
 
-def d(b):
-    if type(b) is int:
-        b = str(b)
-    n = 0
-    l = len(b)-1
-    for i in range(len(b)):
-        d = int(b[i])
-        p = l-i
-        n += d*(2**p)
-    return n
+def bintoalpha(code):
+    letters = 'abcdefghijklmnñopqrstuvwxyz '
+    dec = to_decimal(code)
+    i = letters[dec]
+    return i
