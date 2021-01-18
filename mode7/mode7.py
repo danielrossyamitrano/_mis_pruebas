2b1d3dc2907219a2e3ef5fa56573cb8f85462d03
danielrossyamitrano <32693604Zd>
2021-01-18 16:36:57 -0300

    Proyecto Mode7 + Añadido un "mapa" (una alternación de colores únicamente).

--- mode7/mode7.py
+++ mode7/mode7.py
@@ -1,21 +1,29 @@
 from pygame import draw, Surface
 from itertools import cycle
 
-colors = [(255, 255, 255), (125, 125, 125)]
-cycler = cycle(colors)
 yes = [1, 2, 4, 8, 16, 32, 64, 128]
 y_cycler = cycle(yes)
+
+mapa = [
+    (255, 255, 255),
+    (125, 125, 125),
+    (255, 255, 255),
+    (125, 125, 125),
+    (255, 255, 255),
+    (125, 125, 125),
+    (255, 255, 255),
+    (125, 125, 125),
+]
 
 
 def draw_bg():
     new = Surface((640, 240))
-    color = next(cycler)
     dy = next(y_cycler)
+    i = -1
     for y in range(new.get_height()):
         if y == dy:
+            i += 1
             dy = next(y_cycler)
-            color = next(cycler)
-
-        draw.line(new, color, (0, y), (640, y))
+        draw.line(new, mapa[i], (0, y), (640, y))
 
     return new
