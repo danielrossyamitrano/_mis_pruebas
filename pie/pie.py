5f371616f1a1e54015c17ceb78e963f3ab2a95a1
danielrossyamitrano <danielrossyamitrano@gmail.com>
2021-04-18 19:35:01 -0300

    Pie Chart (update) + Los Handlers ahora se posicionan correctamente y responden a los   eventos rudimentarios del mouse. Por ahora se mueven libremente en   tanto se los arrastre con el mouse. Si bien se generan dos handlers   por arco, al hacer click en uno se seleccionan los dos y son   arrastrados a la vez. Esto es intencional, debido a que al aumentar un   valor decrece otro y visceversa, de manera de que siempre se mantenga   el 100%.

--- pie/pie.py
+++ pie/pie.py
@@ -1,6 +1,6 @@
+from pygame import KEYDOWN, K_ESCAPE, SRCALPHA, MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP
 from pygame import display, event, QUIT, quit, draw
 from pygame.sprite import Sprite, LayeredUpdates
-from pygame import KEYDOWN, K_ESCAPE, SRCALPHA
 from pygame import Surface, transform
 from math import sin, cos, pi
 from sys import exit
@@ -9,28 +9,66 @@ from sys import exit
 
 pantalla = display.set_mode((320, 240))
 screen_rect = pantalla.get_rect()
-values = [
-    # [60, [255, 0, 0, 255]],
-    [36, [0, 255, 0, 255]],
-    # [4, [0, 0, 255, 255]]
-]
 
 
-class Handle(Sprite):
-    def __init__(self, x, y):
+class BaseWidget(Sprite):
+    parent = None
+
+    def __init__(self, parent=None):
         super().__init__()
+        self.parent = parent
+        if hasattr(parent, 'layer'):
+            self.layer = self.parent.layer + 1
+
+    def on_mousebuttondown(self, event):
+        pass
+
+    def on_mousebuttonup(self, event):
+        pass
+
+    def on_mousemotion(self, event):
+        pass
+
+
+class Handle(BaseWidget):
+    pressed = False
+
+    def __init__(self, parent, x, y):
+        super().__init__(parent)
         image = Surface((8, 8), SRCALPHA)
         image.fill((0, 0, 0))
-        # self.image = transform.rotate(image, 45.0)
-        self.image = image
+        self.image = transform.rotate(image, 45.0)
         self.rect = self.image.get_rect(center=(x, y))
 
+    def on_mousebuttondown(self, event):
+        if event.button == 1:
+            self.pressed = True
 
-class Arc(Sprite):
+    def on_mousebuttonup(self, event):
+        if event.button == 1:
+            self.pressed = False
+
+    def on_mousemotion(self, event):
+        dx, dy = event.rel
+        self.rect.move_ip(dx, dy)
+        self.parent.adjust()
+
+    def update(self, *args, **kwargs) -> None:
+        pass
+
+    def __repr__(self):
+        return 'Handler'
+
+
+class Arc(BaseWidget):
+    layer = 1
+    pressed = False
+
     def __init__(self, clr, a, b, cx, cy, r):
         super().__init__()
         self.image = Surface((r * 2, r * 2), SRCALPHA)
         self.rect = self.image.get_rect()
+        self.color = clr
         p = [(self.rect.centerx, self.rect.centery)]
         for n in range(a, b+1):
             x = self.rect.centerx + int(r * cos(n * pi / 180))
@@ -39,28 +77,66 @@ class Arc(Sprite):
 
         draw.polygon(self.image, clr, p)
 
-        self.handle_left = Handle(p[1][0]-self.rect.width, p[1][1])
-        # self.handle_right = Handle(p[-1][0]+self.rect.centerx, p[-1][1]+self.rect.centery, 'right')
+        self.handle_left = Handle(self, p[1][0], p[1][1])
+        self.handle_right = Handle(self, p[-1][0], p[-1][1])
 
-        self.rect.center = (cx, cy)
+        dx = cx-self.rect.centerx
+        dy = cy-self.rect.centery
+        self.displace(dx, dy)
 
+    def displace(self, dx, dy):
+        self.rect.move_ip(dx, dy)
+        self.handle_right.rect.move_ip(dx, dy)
+        self.handle_left.rect.move_ip(dx, dy)
+
+    def adjust(self):
+        pass
+
+    def update(self, *args, **kwargs) -> None:
+        pass
+
+    def __repr__(self):
+        return 'Arc'+str(self.color)
+
+
+values = [
+    [60, [255, 0, 0, 255]],
+    [36, [0, 255, 0, 255]],
+    [4, [0, 0, 255, 255]]
+]
 
 chart = LayeredUpdates()
 arco = 0
 u = 0
 for value, color in values:
     arco += round((value / 100) * 360)
-    arc = Arc(color, u, arco, 50, 50, 74)
-    chart.add(arc, arc.handle_left)
+    arc = Arc(color, u, arco, *screen_rect.center, 74)
+    chart.add(arc, arc.handle_left, arc.handle_right)
     u = arco
 
 while True:
-    events = event.get([KEYDOWN, QUIT])
+    events = event.get([KEYDOWN, QUIT, MOUSEMOTION, MOUSEBUTTONDOWN, MOUSEBUTTONUP])
     for e in events:
         if (e.type == QUIT) or (e.type == KEYDOWN and e.key == K_ESCAPE):
             quit()
             exit()
 
+        if e.type == MOUSEBUTTONDOWN:
+            widgets = chart.get_sprites_at(e.pos)
+            for widget in widgets:
+                widget.on_mousebuttondown(e)
+
+        elif e.type == MOUSEBUTTONUP:
+            widgets = chart.get_sprites_at(e.pos)
+            for widget in widgets:
+                widget.on_mousebuttonup(e)
+
+        elif e.type == MOUSEMOTION:
+            for widget in chart.sprites():
+                if widget.pressed:
+                    widget.on_mousemotion(e)
+
     pantalla.fill((125, 125, 125, 255))
+    chart.update()
     chart.draw(pantalla)
     display.update()
