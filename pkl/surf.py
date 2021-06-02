dfab0d66e299a74bd7715b8aa75fda506680b095
danielrossyamitrano <danielrossyamitrano@gmail.com>
2021-06-01 23:50:23 -0300

    Nueva prueba PKL. Usa el módulo Pickle para guardar un Surface junto a algunos metadatos.

---
+++ pkl/surf.py
@@ -0,0 +1,28 @@
+from pickle import dump, load
+from pygame import Surface, PixelArray
+
+def unpickle_surf(filename):
+    with open(filename,'r+b') as file:
+        d = load(file)
+
+    img = Surface([d['w'],d['h']])
+    px_array = PixelArray(img)
+    i = -1
+    for x in range(d['w']):
+        for y in range(d['h']):
+            i += 1
+            px_array[x,y] = d['pixels'][i]
+            
+    return px_array.make_surface()
+
+def pickle_surf(surface, ruta):
+    pixels =[]
+    w, h = surface.get_size()
+    d = {'w': w, 'h':h}
+    for x in range(w):
+        for y in range(h):
+            pixels.append(pxarray[x,y])
+    d['pixels'] = pixels
+
+    with open(ruta,'w+b') as file:
+        dump(d,file)
