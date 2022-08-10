44d559c46fd0e5f47ed11673fd603989262399e2
danielrossyamitrano <danielrossyamitrano@gmail.com>
2022-08-09 23:45:43 -0300

    Distribution:
    + Ahora es una función completa con opciones. devuelve un diccionario
      con los datos de sistemas (hace incluso más que la galaxysheet de
      artifexian).

--- galaxy/distribution.py
+++ galaxy/distribution.py
@@ -1,28 +1,116 @@
-﻿from math import pi, sin, cos, acos
+﻿from math import pi, sin, cos, acos, floor
+from decimal import Decimal
+from random import choice
 
-radius = 10
-seed = 1
 
-initial_value = (48271 * seed) % (2 ** 31 - 1)
+def stellar_neighbourhood(galactic_radius, location, radius, density, seed=1, home_preference=None):
+    # galactic characteristc
+    galactic_habitable_zone_inner = galactic_radius * 0.47
+    galactic_habitable_zone_outer = galactic_radius * 0.6
 
-total = 17  # Total Star Systems in Stellar Neighbourhood
-r_raw = initial_value
-for i in range(1, total - 1):
+    # stellar neighbourhood
+    if home_preference is not None:
+        warning = "This neighbourhood is uninhabitable"
+        assert galactic_habitable_zone_inner <= location <= galactic_habitable_zone_outer, warning
+    radius = Decimal(radius)
+    stellar_density = round(Decimal(density), 3)
 
-    p_raw = 48271 * r_raw % 2 ** 31 - 1
-    q_raw = 48271 * p_raw % 2 ** 31 - 1
-    r_raw = 48271 * q_raw % 2 ** 31 - 1
+    _density = (stellar_density * (Decimal(4 / 3) * Decimal(pi) * Decimal(pow(radius, 3))))
 
-    p_normal = p_raw / (2 ** 31 - 1)
-    q_normal = q_raw / (2 ** 31 - 1)
-    r_normal = r_raw / (2 ** (31 - 1))
+    # Main sequence stars
+    o = round(_density * Decimal(0.9) * Decimal(0.0000003))
+    b = round(_density * Decimal(0.9) * Decimal(0.0013))
+    a = round(_density * Decimal(0.9) * Decimal(0.006))
+    f = round(_density * Decimal(0.9) * Decimal(0.03))
+    g = round(_density * Decimal(0.9) * Decimal(0.076))
+    k = round(_density * Decimal(0.9) * Decimal(0.121))
+    m = round(_density * Decimal(0.9) * Decimal(0.7645))
 
-    p = p_normal ** (1 / 3) * radius
-    q = q_normal * 2 * pi
-    r = acos(2 * r_normal - 1)
+    # White Dwarfs
+    w = round(_density * Decimal(0.09))
 
-    x = round(p * sin(r) * cos(q), 2)
-    y = round(p * sin(r) * sin(q), 2)
-    z = round(p * cos(r), 2)
+    # Brown Dwarfs
+    d = round(_density / Decimal(2.5))
 
-    print(x, y, z)
+    # Other Stellar Mass Objects
+    other = floor(floor(_density * Decimal(0.01)))
+
+    # Total Stellar Mass Objects in Stellar Neighbourhood
+    total_mass = Decimal(round(sum([o, b, a, f, g, k, m, w, d, other])))
+    stars = ['o']*o+['b']*b+['a']*a+['f']*f+['g']*g+['k']*k+['m']*m+['w']*w+['d']*d+['?']*other
+    binary = round(((total_mass / Decimal(1.58)) * Decimal(0.33)))
+    triple = round(((total_mass / Decimal(1.58)) * Decimal(0.08)))
+    multiple = round(((total_mass / Decimal(1.58)) * Decimal(0.03)))
+    single = round(total_mass - ((binary * 2) + (triple * 3) + (multiple * 4))) - 1
+    total_systems = sum([single, binary, triple, multiple])
+
+    seed = Decimal(seed)
+    divisor = Decimal(2 ** 31 - 1)
+    constante = Decimal(48271)
+    initial_value = (constante * seed) % divisor
+    r_raw = initial_value
+    systems = {}
+    if home_preference is not None:
+        try:
+            systems['Home'] = {"position": (0, 0, 0), 'composition': [stars.pop(stars.index(home_preference))]}
+            delta = 1
+        except ValueError:
+            print(f'There are no {home_preference.capitalize()}-Stars in this Neighbourhood')
+            return
+    else:
+        delta = 0
+
+    for i in range(delta, total_systems + delta):
+        if i <= single:
+            prefix = 'Single'
+            idx = i
+            quantity = 1
+        elif single < i <= single + binary:
+            prefix = 'Binary'
+            idx = i - (single + 1)
+            quantity = 2
+        elif binary < i <= single + binary + triple:
+            prefix = 'Triple'
+            idx = i - ((single + 1) + binary)
+            quantity = 3
+        else:
+            idx = i - ((single + 1) + binary + triple)
+            prefix = 'Multiple'
+            quantity = 4
+
+        p_raw = constante * r_raw % divisor
+        q_raw = constante * p_raw % divisor
+        r_raw = constante * q_raw % divisor
+
+        p_normal = p_raw / divisor
+        q_normal = q_raw / divisor
+        r_normal = r_raw / divisor
+
+        p = (p_normal ** Decimal(1 / 3)) * radius
+        q = q_normal * Decimal(2) * Decimal(pi)
+        r = acos(Decimal(2) * r_normal - Decimal(1))
+
+        x = float(round(p * Decimal(sin(r)) * Decimal(cos(q)), 2))
+        y = float(round(p * Decimal(sin(r)) * Decimal(sin(q)), 2))
+        z = float(round(p * Decimal(cos(r)), 2))
+
+        name = f"{prefix} Star System #{str(idx)}"
+
+        composition = []
+        for _ in range(quantity):
+            if len(stars):
+                star = choice(stars)
+                stars.remove(star)
+                composition.append(star)
+
+        systems[name] = {'position': (x, y, z), 'distance': round(float(p), 2), 'composition': composition}
+    return systems
+
+
+sistemas = stellar_neighbourhood(5000, 2500, 10, 0.004, seed=14528, home_preference='g')
+for sistema in sistemas:
+    print(sistema, sistemas[sistema])
+
+# as a general rule, the greater the distance between the center of the galaxy and the stellar neighbourhood, the
+# less material (aka density) there is to form stars, so the two values should be merged into one variable.
+# it is probably an exponential function.
